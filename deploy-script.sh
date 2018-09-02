56d47894f8871de2df1689bf2c276d8716a6668c
denis-vivies-home <denis-vivies@netcourrier.com>
2018-09-02 19:14:15 +0200

    change way to get workspace dir

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 # Get dirname of this script (repo)
-WORKSPACE=dirname $0
+echo "Getting workspace dir"
+WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Export variables from config
+echo "Exporting variables"
 source ${WORKSPACE}/config/deploy-vars
 
 # Create exportable public/ with hugo
+echo "Generating public/ version"
 hugo
 
+# TODO: add git commit+version+push
+
 # Send content of public to hosting server
+echo "SENDING TO SERVER"
 rsync -av public/* ${DEPLOY_USER}@${DEPLOY_HOST}:/var/www/gnonpy.com
 
 # Remove last public folder
-rm -r public
+echo "Removing public"
+rm -r ${WORKSPACE}/public/
